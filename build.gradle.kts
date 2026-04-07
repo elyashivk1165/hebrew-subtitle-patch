bd5d1a170303574e2f41b5c7b89b997804258009
elyashivk1165 <137767997+elyashivk1165@users.noreply.github.com>
2026-04-07 16:19:10 +0300

    Add build.gradle.kts

---
+++ build.gradle.kts
@@ -0,0 +1,14 @@
+group = "app.revanced"
+version = "1.0.0"
+
+patches {
+    about {
+        name = "Hebrew Subtitle Patch"
+        description = "Adds Hebrew auto-translation to YouTube subtitles"
+        source = "https://github.com/elyashivk1165/hebrew-subtitle-patch"
+        author = "elyashivk1165"
+        contact = ""
+        website = "https://github.com/elyashivk1165/hebrew-subtitle-patch"
+        license = "GNU General Public License v3.0"
+    }
+}
